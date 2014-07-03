bfc2f2a3ce6db8698ed299c78ee043e9ecdb9f8e
Dmitry Akimov <deemidroll@gmail.com>
2014-07-03 12:52:28 +0400

    change interface errors

--- assets_source/js/init.js
+++ assets_source/js/init.js
@@ -2686,6 +2686,12 @@ window.DT = (function (window, document, undefined) {
 // ╚═╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝ ╚═════╝╚══════╝
 ;(function () {
     'use strict';
+    var errors = [
+        '400', '401', '402', '403', '404', '405', '406', '407', '408', '409', '410',
+        '411', '412', '413', '414', '415', '416', '417', '418', '422', '423', '424',
+        '425', '426', '428', '429', '431', '434', '449', '451', '456', '499', '500',
+        '501', '502', '503', '504', '505', '506', '507', '508', '509', '510', '511'],
+        errorsLen = errors.length;
     DT.runApp = function () {
         DT.initSocket();
         if (!document.hasFocus()) {
@@ -2765,7 +2771,7 @@ window.DT = (function (window, document, undefined) {
         }
     });
     DT.$document.on('hit', function (e, data) {
-        $('.error').html('ERROR ' + DT.genRandomFloorBetween(500, 511));
+        $('.error').html(errors[DT.genRandomFloorBetween(0, errorsLen)]);
         $('.hit').css({'display': 'table'}).fadeOut(250);
     });
     DT.$document.on('startGame', function (e, data) {
